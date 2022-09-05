885b03eb15618824822ce48fe2f37e86fc304edc
Mathieu AMBLARD (u118971) <mathieu.amblard@mobi.ch>
2022-09-05 14:14:35 +0200

    chore: setup renovate

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # renovate-17629
 To reproduce https://github.com/renovatebot/renovate/discussions/17629
+
+## Repositories
+
+https://github.com/mathieu-amblard/renovate-17629-test
+
+## Expected Behaviour
+
+After running the Renovate job, I expect to have only one PR with these changes :
+
+org.apache.commons:commons-lang3:3.12.0
+org.springframework.boot:spring-boot-starter:2.6.11
+
+## Actual Behaviour
+
+After running the Renovate job, I have in only one PR with these changes :
+
+org.apache.commons:commons-lang3:3.12.0
---
+++ config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    "$schema": "https://docs.renovatebot.com/renovate-schema.json",
+    "requireConfig": "ignored",
+    "platform": "github",
+    "repositories": ["mathieu-amblard/renovate-17629-test"],
+    "autodiscover": false,
+    "enabledManagers": ["maven"],
+    "packageRules": [
+        {
+            "groupName": "maven",
+            "matchDatasources": ["maven"],
+            "separateMajorMinor": false,
+            "separateMinorPatch": false,
+            "fetchReleaseNotes": false
+        },
+        {
+            "groupName": "maven",
+            "matchPackagePrefixes": ["org.springframework:", "org.springframework.boot:"],
+            "matchUpdateTypes": ["major", "minor"],
+            "enabled": false
+        }
+    ]
+};
